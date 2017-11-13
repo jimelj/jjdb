fb2a56e3c5a3c39f95bebd18fc821382b676b7b9
Jimel Joseph <jimelj@gmail.com>
2017-11-13 18:42:54 -0500

    setting state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = "Welcome to React"
 
 class App extends Component {
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome />
+          <Welcome text="Welcome to Using Props" toggle={this.state.toggle} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.toggle && 
+          <p>This should show and hide</p>
+        }
+        <button onClick={this.toggle}>Show / Hide</button>
       </div>
     );
   }
@@ -22,8 +36,10 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
+    const { text, toggle } = this.props;
+    console.log(toggle);
     return (
-      <h1 className="App-title">{welcome}</h1>
+      <h1 className="App-title">{text}</h1>
     )
   }
 }
